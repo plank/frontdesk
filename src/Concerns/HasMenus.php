363ece3d4c5b918890549a640c1003c072142982
Massimo Triassi <massimo@plankdesign.com>
2023-10-23 12:41:16 -0400

    chore: #1 remove unused code, buildMenu was move to menu model

--- src/Concerns/HasMenus.php
+++ src/Concerns/HasMenus.php
@@ -2,7 +2,6 @@
 
 namespace Plank\Frontdesk\Concerns;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Plank\Frontdesk\Models\Menu;
@@ -17,19 +16,5 @@ trait HasMenus
         /** @var $this Model */
         return $this->morphToMany(Menu::class, 'menuable')
             ->orderByPivot('order');
-    }
-
-    public function buildMenu($id = null, $depth = 3): string
-    {
-        $q = $this->menus()->with(['hyperlinks' => function (Builder $q) {
-            $q->orderBy('order');
-        }]);
-
-        if (!empty($id)) {
-            $menu = $q->first()->hyperlinks;
-        } else {
-            $menu = $q->find($id)->hyperlinks;
-        }
-
     }
 }
