5a2dba7d30ff2f9b9400e0821b93f082f3e597c8
Massimo Triassi <massimo@plankdesign.com>
2023-10-23 15:56:16 -0400

    feat: #1 add ability for user to configure factory location to ease application testing

--- config/frontdesk.php
+++ config/frontdesk.php
@@ -2,10 +2,16 @@
 
 use Plank\Frontdesk\Models\Hyperlink;
 use Plank\Frontdesk\Models\Menu;
+use Plank\Frontdesk\Tests\Database\Factories\HyperlinkFactory;
+use Plank\Frontdesk\Tests\Database\Factories\MenuFactory;
 
 return [
     'models' => [
         'hyperlink' => Hyperlink::class,
         'menu' => Menu::class
+    ],
+    'factories' => [
+        'hyperlink' => HyperlinkFactory::class,
+        'menu' => MenuFactory::class
     ]
 ];
