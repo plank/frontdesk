2542ff4321c36e8227e0a8fd2f9fda19b7800c6b
Massimo Triassi <massimo@plankdesign.com>
2023-10-23 12:40:19 -0400

    chore: #1 remove build function, to be added in a different issue

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -4,8 +4,6 @@ namespace Plank\Frontdesk\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Collection;
-use Plank\Frontdesk\Contracts\HyperlinkInterface;
 use Plank\Frontdesk\Contracts\MenuInterface;
 
 class Menu extends Model implements MenuInterface
@@ -16,18 +14,5 @@ class Menu extends Model implements MenuInterface
     {
         $hyperlinkModel = config('frontdesk.models.hyperlink');
         return $this->hasMany($hyperlinkModel);
-    }
-
-    public function build($depth = 3): Collection
-    {
-        $links = $this->hyperlinks;
-
-        // order links so that the array is index so that a parent appears and is proceeded by its children, simply using ->sortBy('parent_id') does not produce desired results
-        $links = $links->sortBy(function (HyperlinkInterface $link) {
-            // sort so that we get parent, child, child, child, new parent, child, child
-            return $link->parent_id . $link->id;
-        });
-
-        return $links;
     }
 }
