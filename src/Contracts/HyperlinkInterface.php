b5b3ab192000873b7dff0b88780b5b5414bdcf52
Massimo <m-triassi@users.noreply.github.com>
2023-10-24 09:33:25 -0400

    docs: #1 add type hinting for better autocomplete
    Co-authored-by: Kurt <kfriars@gmail.com>

--- src/Contracts/HyperlinkInterface.php
+++ src/Contracts/HyperlinkInterface.php
@@ -9,6 +9,10 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 /**
  * @property int|string $id
  * @property int|string $parent_id
+ * @property-read Collection<Linkable> $linkable
+ * @property-read Collection<Menu> $menu
+ * @property-read Model&HyperlinkInterface|null $parent
+ * @property-read Collection<Model&HyperlinkInterface> $children
  */
 interface HyperlinkInterface
 {
