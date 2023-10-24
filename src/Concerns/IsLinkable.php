973816151b112a49c2ac8d6dc93851325652ff4f
Massimo <m-triassi@users.noreply.github.com>
2023-10-24 09:32:47 -0400

    docs: #1 add type hinting for better autocomplete
    Co-authored-by: Kurt <kfriars@gmail.com>

--- src/Concerns/IsLinkable.php
+++ src/Concerns/IsLinkable.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Plank\Frontdesk\Models\Hyperlink;
 
+/**
+ * @mixin \Illuminate\Database\Eloquent\Model
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection<Hyperlink> $hyperlinks
+ */
 trait IsLinkable
 {
     public function hyperlinks(): MorphMany
