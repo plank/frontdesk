d9b408065ea71a82c4fdc2464731b55c046f4c00
Massimo <m-triassi@users.noreply.github.com>
2023-10-24 09:32:38 -0400

    docs: #1 add type hinting for better autocomplete
    Co-authored-by: Kurt <kfriars@gmail.com>

--- src/Concerns/HasMenus.php
+++ src/Concerns/HasMenus.php
@@ -5,7 +5,11 @@ namespace Plank\Frontdesk\Concerns;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Plank\Frontdesk\Models\Menu;
-
+/**
+ * @mixin \Illuminate\Database\Eloquent\Model
+ *
+ * @property-read Collection<Menu> $menus
+ */
 trait HasMenus
 {
     /**
