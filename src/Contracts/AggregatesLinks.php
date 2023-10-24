253470f8001f50e8c750ab9a7b9abc482847476f
Massimo Triassi <massimo@plankdesign.com>
2023-10-24 14:34:21 -0400

    docs: #1 add @property-read to aid in type hinting

--- src/Contracts/AggregatesLinks.php
+++ src/Contracts/AggregatesLinks.php
@@ -2,8 +2,14 @@
 
 namespace Plank\Frontdesk\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property-read Collection<Model&LinksToContent> $hyperlinks
+ * Could add $navs to this interface if you decide to implement it
+ */
 interface AggregatesLinks
 {
     public function hyperlinks(): HasMany;
