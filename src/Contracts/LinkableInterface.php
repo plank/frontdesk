b3659f9647aae98d19fe0ecab0f4b22694d58207
Massimo <m-triassi@users.noreply.github.com>
2023-10-24 09:33:45 -0400

    docs: #1 add type hinting for better autocomplete
    Co-authored-by: Kurt <kfriars@gmail.com>

--- src/Contracts/LinkableInterface.php
+++ src/Contracts/LinkableInterface.php
@@ -3,7 +3,11 @@
 namespace Plank\Frontdesk\Contracts;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-
+/**
+ * @property-read Collection<Model&LinksToContent> $hyperlinks
+ * @property-read string $linkTitle
+ * @property-read string $linkUrl
+ */
 interface LinkableInterface
 {
     public function hyperlinks(): MorphMany;
